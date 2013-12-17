22ef277844b07b7c28370be83dd7fa585b45c4fa
linyupark <linyupark@gmail.com>
2013-12-17 13:00:55 +0800

    避免跟系统的discover package功能冲突，删除部分功能

--- LinkOpener.py
+++ LinkOpener.py
@@ -24,7 +24,7 @@
 import sublime
 import sublime_plugin
 import webbrowser
-import re
+# import re
 
 import sys
 ST3 = sys.version_info >= (3, 3)
@@ -34,144 +34,142 @@ if ST3:
 else:
   import urllib2
 
-junk = r"(?:\s|\;|\)|\]|\[|\{|\}|,|\"|'|:|\<|$|\.\s)"
-head = r"(?:(?:(?:http|https|ftp)://(?:\S+?))|(?:\bwww\.\S+?))"
+# junk = r"(?:\s|\;|\)|\]|\[|\{|\}|,|\"|'|:|\<|$|\.\s)"
+# head = r"(?:(?:(?:http|https|ftp)://(?:\S+?))|(?:\bwww\.\S+?))"
 
-regexFindUrl = "(" + head + ")" + junk
-findUrl = re.compile(regexFindUrl, re.I)
-findUrlExact = re.compile("^" + regexFindUrl + "$", re.I)
-findJunk = re.compile(junk + "$", re.I)
+# regexFindUrl = "(" + head + ")" + junk
+# findUrl = re.compile(regexFindUrl, re.I)
+# findUrlExact = re.compile("^" + regexFindUrl + "$", re.I)
+# findJunk = re.compile(junk + "$", re.I)
 
-def settings():
- return sublime.load_settings('linkopener.sublime-settings')
-
-
-def isLink(text):
-    return text[:7] is "http://" or text[:7] is "http://"
+# def isLink(text):
+#     return text[:7] is "http://" or text[:7] is "http://"
 
 
-def findLinks(text):
-    return re.findall(findUrl, text)
+# def findLinks(text):
+#     return re.findall(findUrl, text)
 
 
-def isSelectionLink(text):
-    return findUrlExact.match(text) is not None
+# def isSelectionLink(text):
+#     return findUrlExact.match(text) is not None
 
 
-def cleanLink(link):
-    return re.sub(findJunk, '', link)
+# def cleanLink(link):
+#     return re.sub(findJunk, '', link)
 
 
-def fixLink(link):
-    if link[:4] is "www.":
-        link = "http://" + link
+# def fixLink(link):
+#     if link[:4] is "www.":
+#         link = "http://" + link
 
-    return link
-
-
-def statusInfo(opened):
-    if len(opened) > 0:
-        message = "Opening link"
-        if len(opened) > 1:
-            message += "s"
-
-        message += ": "
-
-        for link in opened:
-            message += " [" + link + "]"
-
-    else:
-        message = "No link found"
-
-    return "LinkOpener > " + message
+#     return link
 
 
-class OpenUrlCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        selection = self.view.sel()
-        links = []
+# def statusInfo(opened):
+#     if len(opened) > 0:
+#         message = "Opening link"
+#         if len(opened) > 1:
+#             message += "s"
 
-        for select in selection:
-            contents = self.view.substr(select)
-            links += findLinks(contents)
+#         message += ": "
 
-        opened = []
-        for link in links:
-            link = cleanLink(link)
-            link = fixLink(link)
+#         for link in opened:
+#             message += " [" + link + "]"
 
-            if link is not None and link not in opened:
-                webbrowser.open(link, 1, settings().get('raise_window'))
-                opened.append(link)
+#     else:
+#         message = "No link found"
 
-            if settings().get('first_link_only'):
-                break
-
-        sublime.status_message(statusInfo(opened))
+#     return "LinkOpener > " + message
 
 
-class SelectNextUrlCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        selection = self.view.sel()
-        finalSelection = None
-        index = -1
+# class OpenUrlCommand(sublime_plugin.TextCommand):
+#     settings = sublime.load_settings('linkopener.sublime-settings')
+#     def run(self, edit):
+#         selection = self.view.sel()
+#         links = []
 
-        for select in selection:
-            contents = self.view.substr(select)
+#         for select in selection:
+#             contents = self.view.substr(select)
+#             links += findLinks(contents)
 
-            if select.begin() == select.end():
-                select = sublime.Region(select.begin(), self.view.size())
-            elif len(contents) > 0 and isSelectionLink(contents):
-                select = sublime.Region(select.end(), self.view.size())
+#         opened = []
+#         for link in links:
+#             link = cleanLink(link)
+#             link = fixLink(link)
 
-            contents = self.view.substr(select)
-            link = findLinks(contents)
-            index += 1
+#             if link is not None and link not in opened:
+#                 webbrowser.open(link, 1, self.settings.get('raise_window'))
+#                 opened.append(link)
 
-            if len(link) > 0:
-                finalSelection = select
-                break
+#             if settings.get('first_link_only'):
+#                 break
 
-        if len(selection) is 0 or len(link) is 0:
-            select = sublime.Region(0, self.view.size())
-            contents = self.view.substr(select)
-            link = findLinks(contents)
-
-            if len(link) > 0:
-                finalSelection = select
-
-        if len(link) > 0:
-            if type(link[0]) is tuple:
-                link[0] = link[0][0]
-
-            result = self.view.find(link[0], finalSelection.begin(), sublime.LITERAL)
-
-            region = sublime.Region(result.begin(), result.end())
-
-            selection.clear()
-            selection.add(region)
+#         sublime.status_message(statusInfo(opened))
 
 
-class SelectAllUrlsCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        selection = self.view.sel()
+# class SelectNextUrlCommand(sublime_plugin.TextCommand):
+#     def run(self, edit):
+#         selection = self.view.sel()
+#         finalSelection = None
+#         index = -1
 
-        select = sublime.Region(0, self.view.size())
-        contents = self.view.substr(select)
-        link = findLinks(contents)
+#         for select in selection:
+#             contents = self.view.substr(select)
 
-        if len(link) > 0:
-            link = map(re.escape, link)
-            links = "|".join(link)
+#             if select.begin() == select.end():
+#                 select = sublime.Region(select.begin(), self.view.size())
+#             elif len(contents) > 0 and isSelectionLink(contents):
+#                 select = sublime.Region(select.end(), self.view.size())
 
-            result = self.view.find_all(links)
-            selection.clear()
+#             contents = self.view.substr(select)
+#             link = findLinks(contents)
+#             index += 1
 
-            for reg in result:
-                region = sublime.Region(reg.begin(), reg.end())
-                selection.add(region)
+#             if len(link) > 0:
+#                 finalSelection = select
+#                 break
+
+#         if len(selection) is 0 or len(link) is 0:
+#             select = sublime.Region(0, self.view.size())
+#             contents = self.view.substr(select)
+#             link = findLinks(contents)
+
+#             if len(link) > 0:
+#                 finalSelection = select
+
+#         if len(link) > 0:
+#             if type(link[0]) is tuple:
+#                 link[0] = link[0][0]
+
+#             result = self.view.find(link[0], finalSelection.begin(), sublime.LITERAL)
+
+#             region = sublime.Region(result.begin(), result.end())
+
+#             selection.clear()
+#             selection.add(region)
+
+
+# class SelectAllUrlsCommand(sublime_plugin.TextCommand):
+#     def run(self, edit):
+#         selection = self.view.sel()
+
+#         select = sublime.Region(0, self.view.size())
+#         contents = self.view.substr(select)
+#         link = findLinks(contents)
+
+#         if len(link) > 0:
+#             link = map(re.escape, link)
+#             links = "|".join(link)
+
+#             result = self.view.find_all(links)
+#             selection.clear()
+
+#             for reg in result:
+#                 region = sublime.Region(reg.begin(), reg.end())
+#                 selection.add(region)
 
 class SearchTermCommand(sublime_plugin.TextCommand):
+    settings = sublime.load_settings('linkopener.sublime-settings')
     def run(self, edit):
         selection = self.view.sel()
         select = sublime.Region(0, self.view.size())
@@ -189,12 +187,13 @@ class SearchTermCommand(sublime_plugin.TextCommand):
 
 
         for term in terms:
-            link = settings().get('search_url').replace('%s', term)
+            link = self.settings.get('search_url').replace('%s', term)
 
             webbrowser.open(link, 1, True)
 
 
 class AskTermCommand(sublime_plugin.TextCommand):
+    settings = sublime.load_settings('linkopener.sublime-settings')
     def run(self, edit):
         selection = self.view.sel()
         select = sublime.Region(0, self.view.size())
@@ -212,12 +211,13 @@ class AskTermCommand(sublime_plugin.TextCommand):
 
 
         for term in terms:
-            link = settings().get('ask_url').replace('%s', term)
+            link = self.settings.get('ask_url').replace('%s', term)
 
             webbrowser.open(link, 1, True)
 
 
 class TransTermCommand(sublime_plugin.TextCommand):
+    settings = sublime.load_settings('linkopener.sublime-settings')
     def run(self, edit):
         selection = self.view.sel()
         select = sublime.Region(0, self.view.size())
@@ -235,7 +235,7 @@ class TransTermCommand(sublime_plugin.TextCommand):
 
 
         for term in terms:
-            link = settings().get('trans_url').replace('%s', term)
+            link = self.settings.get('trans_url').replace('%s', term)
 
             webbrowser.open(link, 1, True)
 
